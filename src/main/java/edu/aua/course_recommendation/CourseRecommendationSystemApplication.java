9a104402cf4c65c8397efa614124683e9f615109
Ed-Harutyunyan <edgar.harutiunyan@icloud.com>
2025-01-28 12:54:08 +0400

    initial commit

---
+++ src/main/java/edu/aua/course_recommendation/CourseRecommendationSystemApplication.java
@@ -0,0 +1,13 @@
+package edu.aua.course_recommendation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseRecommendationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseRecommendationSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/aua/course_recommendation/CourseRecommendationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.aua.course_recommendation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseRecommendationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
